6350c0283e5dbf572d66bd355a7e2a86def78a70
Samil Akpinar <samilakpinar6@gmail.com>
2020-08-08 23:54:10 +0300

    React-Native Navigation

--- App.js
+++ App.js
@@ -1,28 +1,9 @@
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
+import React, { useState } from 'react';
+import Router from './src/Router';
+const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <Router />
+  )
 }
 
 export default App;
